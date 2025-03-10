7764fcbcadbb358a8041048e59d133bf31890dd1
தமிழ்நேரம் <anishprabu.t@gmail.com>
2025-03-10 05:01:46 +0100

    Translated using Weblate (Tamil)
    Currently translated at 100.0% (52 of 52 strings)
    
    Translation: Tracks/Sailfish application
    Translate-URL: https://hosted.weblate.org/projects/tracks/harbour-tracks/ta/

--- translations/harbour-tracks-ta.ts
+++ translations/harbour-tracks-ta.ts
@@ -5,229 +5,229 @@
     <name>AboutPage</name>
     <message>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>பற்றி</translation>
     </message>
     <message>
         <source>Tracks</source>
-        <translation type="unfinished"></translation>
+        <translation>தடங்கள்</translation>
     </message>
     <message>
         <source>Version %1</source>
-        <translation type="unfinished"></translation>
+        <translation>பதிப்பு %1</translation>
     </message>
     <message>
         <source>Tracks is a simple client for the Tracks todo application.</source>
-        <translation type="unfinished"></translation>
+        <translation>தடங்கள் என்பது தடங்கள்செய்யவேண்டிய பயன்பாட்டிற்கான எளிய வாங்குநர் ஆகும்.</translation>
     </message>
     <message>
         <source>Thanking:</source>
-        <translation type="unfinished"></translation>
+        <translation>நன்றி:</translation>
     </message>
     <message>
         <source>– J. Lavoie (German and French translation)</source>
-        <translation type="unfinished"></translation>
+        <translation>- சே. லாவோய் (செர்மன் மற்றும் பிரஞ்சு மொழிபெயர்ப்பு)</translation>
     </message>
     <message>
         <source>– Allan Nordhøy (Norwegian Bokmål translation)</source>
-        <translation type="unfinished"></translation>
+        <translation>- ஆலன் நோர்தே (நோர்வே போக்மால் மொழிபெயர்ப்பு)</translation>
     </message>
     <message>
         <source>– Алексей Свистунов (Russian translation)</source>
-        <translation type="unfinished"></translation>
+        <translation>- அலெக்சி ச்விச்டுனோவ் (ரச்ய மொழிபெயர்ப்பு)</translation>
     </message>
     <message>
         <source>GitHub (source codes and issues)</source>
-        <translation type="unfinished"></translation>
+        <translation>அறிவிலிமையம் (மூல குறியீடுகள் மற்றும் சிக்கல்கள்)</translation>
     </message>
     <message>
         <source>Hosted Tracks application</source>
-        <translation type="unfinished"></translation>
+        <translation>புரவலன் செய்யப்பட்ட தடங்கள் பயன்பாடு</translation>
     </message>
     <message>
         <source>Tracks web page</source>
-        <translation type="unfinished"></translation>
+        <translation>வலைப்பக்கத்தைக் கண்காணிக்கிறது</translation>
     </message>
     <message>
         <source>– Jyri-Petteri ”ZeiP” Paloposki (author)</source>
-        <translation type="unfinished"></translation>
+        <translation>-சிரி-பெட்டரி “சீப்” பாலோபோச்கி (ஆசிரியர்)</translation>
     </message>
     <message>
         <source>– Burak Hüseyin Ekseli and Oğuz Ersen (Turkish translation)</source>
-        <translation type="unfinished"></translation>
+        <translation>- புராக் எசின் எகெலி மற்றும் ஓசுச் எர்சன் (துருக்கிய மொழிபெயர்ப்பு)</translation>
     </message>
 </context>
 <context>
     <name>AddForm</name>
     <message>
         <source>Add task</source>
-        <translation type="unfinished"></translation>
+        <translation>பணியைச் சேர்க்கவும்</translation>
     </message>
     <message>
         <source>Task description</source>
-        <translation type="unfinished"></translation>
+        <translation>பணி விளக்கம்</translation>
     </message>
     <message>
         <source>Note</source>
-        <translation type="unfinished"></translation>
+        <translation>குறிப்பு</translation>
     </message>
     <message>
         <source>Context</source>
-        <translation type="unfinished"></translation>
+        <translation>சூழல்</translation>
     </message>
     <message>
         <source>Project</source>
-        <translation type="unfinished"></translation>
+        <translation>திட்டம்</translation>
     </message>
     <message>
         <source>Due date</source>
-        <translation type="unfinished"></translation>
+        <translation>இரண்டு தேதிகள்</translation>
     </message>
     <message>
         <source>Select</source>
-        <translation type="unfinished"></translation>
+        <translation>தேர்ந்தெடு</translation>
     </message>
     <message>
         <source>Show from</source>
-        <translation type="unfinished"></translation>
+        <translation>இருந்து காட்டு</translation>
     </message>
     <message>
         <source>Tracks task added</source>
-        <translation type="unfinished"></translation>
+        <translation>தடங்கள் பணி சேர்க்கப்பட்டது</translation>
     </message>
     <message>
         <source>Tracks task adding failed</source>
-        <translation type="unfinished"></translation>
+        <translation>தடங்கள் பணி சேர்க்கை தோல்வியடைந்தது</translation>
     </message>
     <message>
         <source>None</source>
-        <translation type="unfinished"></translation>
+        <translation>எதுவுமில்லை</translation>
     </message>
     <message>
         <source>Task %1 added to context %2.</source>
-        <translation type="unfinished"></translation>
+        <translation>பணி %1 சூழல் %2 இல் சேர்க்கப்பட்டது.</translation>
     </message>
     <message>
         <source>Adding task %1 failed.</source>
-        <translation type="unfinished"></translation>
+        <translation>பணி %1 ஐ சேர்ப்பது தோல்வியடைந்தது.</translation>
     </message>
     <message>
         <source>Tag</source>
-        <translation type="unfinished"></translation>
+        <translation>குறிச்சொல்</translation>
     </message>
 </context>
 <context>
     <name>ContextList</name>
     <message>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>அமைப்புகள்</translation>
     </message>
     <message>
         <source>Add task</source>
-        <translation type="unfinished"></translation>
+        <translation>பணியைச் சேர்க்கவும்</translation>
     </message>
     <message>
         <source>Refresh</source>
-        <translation type="unfinished"></translation>
+        <translation>புதுப்பிப்பு</translation>
     </message>
     <message>
         <source>Contexts</source>
-        <translation type="unfinished"></translation>
+        <translation>சூழல்கள்</translation>
     </message>
     <message>
         <source>Nothing here</source>
-        <translation type="unfinished"></translation>
+        <translation>இங்கே எதுவும் இல்லை</translation>
     </message>
     <message>
         <source>Check your settings</source>
-        <translation type="unfinished"></translation>
+        <translation>உங்கள் அமைப்புகளை சரிபார்க்கவும்</translation>
     </message>
     <message>
         <source>Scroll to top</source>
-        <translation type="unfinished"></translation>
+        <translation>மேலே உருட்டவும்</translation>
     </message>
     <message>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>பற்றி</translation>
     </message>
 </context>
 <context>
     <name>CoverPage</name>
     <message>
         <source>Tracks</source>
-        <translation type="unfinished"></translation>
+        <translation>தடங்கள்</translation>
     </message>
 </context>
 <context>
     <name>SettingsPage</name>
     <message>
         <source>Save</source>
-        <translation type="unfinished"></translation>
+        <translation>சேமி</translation>
     </message>
     <message>
         <source>Tracks base URL</source>
-        <translation type="unfinished"></translation>
+        <translation>அடிப்படை முகவரி ஐ கண்காணிக்கிறது</translation>
     </message>
     <message>
         <source>Username</source>
-        <translation type="unfinished"></translation>
+        <translation>பயனர்பெயர்</translation>
     </message>
     <message>
         <source>Password</source>
-        <translation type="unfinished"></translation>
+        <translation>கடவுச்சொல்</translation>
     </message>
 </context>
 <context>
     <name>TaskList</name>
     <message>
         <source>Refresh</source>
-        <translation type="unfinished"></translation>
+        <translation>புதுப்பிப்பு</translation>
     </message>
     <message>
         <source>Nothing here</source>
-        <translation type="unfinished"></translation>
+        <translation>இங்கே எதுவும் இல்லை</translation>
     </message>
     <message>
         <source>Check your settings</source>
-        <translation type="unfinished"></translation>
+        <translation>உங்கள் அமைப்புகளை சரிபார்க்கவும்</translation>
     </message>
     <message>
         <source>Marking as done</source>
-        <translation type="unfinished"></translation>
+        <translation>முடிந்தபடி குறிப்பது</translation>
     </message>
     <message>
         <source>Task %1 set done.</source>
-        <translation type="unfinished"></translation>
+        <translation>பணி %1 தொகுப்பு முடிந்தது.</translation>
     </message>
     <message>
         <source>Tracks task set done</source>
-        <translation type="unfinished"></translation>
+        <translation>கண்காணிப்புகள் பணி தொகுப்பு முடிந்தது</translation>
     </message>
     <message>
         <source>Setting task %1 done failed.</source>
-        <translation type="unfinished"></translation>
+        <translation>பணி %1 முடிந்தது தோல்வியுற்றது.</translation>
     </message>
     <message>
         <source>Tracks task setting failed</source>
-        <translation type="unfinished"></translation>
+        <translation>கண்காணிப்புகள் பணி அமைப்பு தோல்வியடைந்தது</translation>
     </message>
     <message>
         <source>Scroll to top</source>
-        <translation type="unfinished"></translation>
+        <translation>மேலே உருட்டவும்</translation>
     </message>
     <message>
         <source>Actions in %1</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 இல் செயல்கள்</translation>
     </message>
     <message>
         <source>Add task</source>
-        <translation type="unfinished"></translation>
+        <translation>பணியைச் சேர்க்கவும்</translation>
     </message>
 </context>
 <context>
     <name>harbour-tracks</name>
     <message>
         <source>None</source>
-        <translation type="unfinished"></translation>
+        <translation>எதுவுமில்லை</translation>
     </message>
 </context>
 </TS>
